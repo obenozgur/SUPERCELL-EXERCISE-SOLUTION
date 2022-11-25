f0b3c8a950ff77835c65d41f0039404f7a719970
Oben Ozgur <ozguro18@itu.edu.tr>
2022-11-25 20:34:39 +0300

    Implement UpdateCommand data conversion

--- UpdateCommand.java
+++ UpdateCommand.java
@@ -3,16 +3,22 @@ import org.json.simple.JSONObject;
 public class UpdateCommand implements Command 
 {
     private String username;
-    
+    private Integer timestamp;
+
 
     public UpdateCommand(JSONObject jsonObject)
     {
-        
+        username = (String) jsonObject.get("user");
+        timestamp = (Integer) jsonObject.get("timestamp");
+        //TODO convert values Java data
     }
 
     @Override
     public void process()
     {
-        
+        if (Database.isNewUser(username))
+        {
+            //TODO add user to database
+        }
     }
 }
