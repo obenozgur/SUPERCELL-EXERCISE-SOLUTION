def1ad66d72a5de5ce8a3dab25718371f745873d
Oben Ozgur <ozguro18@itu.edu.tr>
2022-11-25 20:21:31 +0300

    Add user friend methods

--- User.java
+++ User.java
@@ -1,10 +1,43 @@
 import java.util.*;
 
 public class User {
-    private String username;
-    private HashSet<String> friends;
-    private int timestamp;
-    private HashMap<String, String> values;
+	private String username;
+	private HashSet<String> friends;
+	private int timestamp;
+	private LinkedHashMap<String, String> values;
 
-    public String getUsername() {return username;}
+	public User(String username)
+	{
+		this.username = username;
+		this.friends = new HashSet<String>(); 
+		this.timestamp = 0;
+		this.values = new LinkedHashMap<String, String>();
+	}
+
+	public String getUsername() 
+	{
+		return username;
+	}
+
+	public boolean equals(Object obj) 
+	{
+		if (obj == this) {
+			return true;
+		}
+		if (!(obj instanceof User)) {
+			return false;
+		}
+		User user = (User) obj;
+		return username.equals(user.username);
+	}
+
+	public void addFriend(String username) 
+	{
+		friends.add(username);
+	}
+	
+	public void deleteFriend(String username) 
+	{
+		friends.remove(username);
+	}
 }
