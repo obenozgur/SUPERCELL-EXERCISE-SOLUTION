54458d527c3ba5888e6c085db59cace85ab098dc
Oben Ozgur <ozguro18@itu.edu.tr>
2022-11-25 11:21:51 +0300

    Update tests

---
+++ User.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class User {
+    private String username;
+    private HashSet<String> friends;
+    private int timestamp;
+    private HashMap<String, String> values;
+
+    public String getUsername() {return username;}
+}
